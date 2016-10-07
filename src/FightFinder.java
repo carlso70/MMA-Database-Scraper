a517adf9a9cb728b133e7b83ddc7dc120e7326d8
James Carlson <carlso70@purdue.edu>
2016-10-07 10:11:52 -0400

    Crawler is working now within the weight classes adding the fighters to a temp list

--- src/FightFinder.java
+++ src/FightFinder.java
@@ -47,42 +47,49 @@ public class FightFinder {
 					+ "div.content-section > div > div.main-section > div.tab-content > "
 					+ "table > tbody > tr.fighter > td > div.cell-inner");
 
-			System.out.println(infoCells);
 			int cellCounter = 0;
 			for (Element cell : infoCells) {
 				if (cellCounter >= 4) {
 					Fighter f = new Fighter(name, nickname, height, weight, wins, losses, draws);
-					f.getName();
 					fighters.add(f);
 					cellCounter = 0;
 				}
-				if (cell.select("div.fighter-info") != null) {
-					name = cell.select("div.fighter-info > div > a").text().toString();
-					nickname = cell.select("div.fighter-nickname").text().toString();
-					System.out.println("name at counter - " + cellCounter);
+				if (cell.select("div.fighter-info > div > a.fighter-name") != null && cellCounter == 0) {
+					if (cell.select("div.fighter-info > div > a.fighter-name").text().toString().length() <= 0)
+						continue;
+					else
+						name = cell.select("div.fighter-info > div > a.fighter-name").text().toString();
+					if (cell.select("div.fighter-nickname").text().toString().length() != 0) {
+						nickname = cell.select("div.fighter-nickname").text().toString();
+					}
 					cellCounter++;
 				}
-				if (cell.select("div.sub-txt") != null && cell.select("div.sub-txt").text().toString().contains("W")) {
+				if (cell.select("div.sub-txt") != null && cell.select("div.sub-txt").text().toString().contains("W")
+						&& cellCounter == 1) {
 					String[] record = cell.select("div.main-txt").text().toString().split("-|\\,");
 					wins = Integer.parseInt(record[0]);
 					losses = Integer.parseInt(record[1]);
 					draws = Integer.parseInt(record[2]);
 
-					System.out.println("wins/losses/draws at counter - " + cellCounter);
 					cellCounter++;
 				}
-				if (cell.select("div.sub-txt") != null && cell.select("div.sub-txt").text().toString().contains("cm")) {
+				if (cell.select("div.sub-txt") != null && cell.select("div.sub-txt").text().toString().contains("cm")
+						&& cellCounter == 2) {
 					String[] cm = cell.select("div.sub-txt").text().toString().split(" ");
 					height = Integer.parseInt(cm[1]);
-					System.out.println("height at counter - " + cellCounter);
 					cellCounter++;
 				}
-				if (cell.select("div.sub-txt") != null && cell.select("div.sub-txt").text().toString().contains("kg")) {
+				if (cell.select("div.sub-txt") != null && cell.select("div.sub-txt").text().toString().contains("kg")
+						&& cellCounter == 3) {
 					String[] lbs = cell.select("div.main-txt").text().toString().split(" ");
 					weight = Integer.parseInt(lbs[0]);
-					System.out.println("weight at counter - " + cellCounter);
 					cellCounter++;
 				}
+			}
+
+			for (Fighter f : fighters) {
+				System.out.println(f.getName() + " has this many wins " + f.getWins());
+
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
--- src/Fighter.java
+++ src/Fighter.java
@@ -3,6 +3,7 @@ public class Fighter {
 	private String name;
 	private String nickname;
 	private int weight;
+	private int height;
 	private int wins, losses, draws;
 
 	// Height is in cm, weight is in lbs
@@ -10,6 +11,7 @@ public class Fighter {
 		this.name = name;
 		this.nickname = nickname;
 		this.weight = weight;
+		this.height = height;
 		this.wins = wins;
 		this.losses = losses;
 		this.draws = draws;
@@ -17,16 +19,20 @@ public class Fighter {
 	
 	public Fighter() {}
 	
+	// Setters
 	public void setName(String name) { this.name = name; }
 	public void setNickname(String nickname) { this.nickname = nickname; }
 	public void setWeight(int weight) { this.weight = weight; }
 	public void setWins(int wins) { this.wins = wins; }
 	public void setLosses(int losses) { this.losses = losses; }
 	public void setDraws(int draws) { this.draws = draws; }
+	public void setHeight(int height) { this.height = height; }
 	
+	// Getters
 	public String getName() { return this.name; }
 	public String getNickname() { return this.nickname; }
 	public int getWeight() { return this.weight; }
+	public int getHeight() { return this.height; }
 	public int getWins() { return this.wins; }
 	public int getLosses() { return this.losses; }
 	public int getDraws() { return this.draws; }
